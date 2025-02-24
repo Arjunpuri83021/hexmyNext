a3c2f6c54c415130aeba314b3192d02f7727aadc
arjun puri <arjunpuri991@gmail.com>
2025-02-23 20:45:09 -0800

    new data uploaded

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +19,9 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="google-site-verification" content="mCwmvJm_aIpaaXKbGYE35hWFSsZeSMlv7e_qLy8-Tns" />
+      </Head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
